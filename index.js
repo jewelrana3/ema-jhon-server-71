3af5e9d46525f0bbc43e20d46cef976d392e6264
jewelrana3 <juyelrana7752@gmail.com>
2023-05-16 11:59:37 +0600

    page

--- index.js
+++ index.js
@@ -28,12 +28,25 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection = client.db('emaJohnDB').collection('products')
+
+    app.get('/products',async(req,res)=>{
+        console.log(req.query)
+        const result = await productCollection.find().toArray()
+        res.send(result) 
+    });
+
+    app.get('/totalProducts',async(req,res)=>{
+        const result = await productCollection.estimatedDocumentCount();
+        res.send({totalProduct:result})
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
